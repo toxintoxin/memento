e7e1f85aca7f480532271475323812096d27746c
toxintoxin <839868794@qq.com>
2025-08-30 19:21:14 +0800

    add helpers

---
+++ helpers/deploy.sh
@@ -0,0 +1,25 @@
+# activate venv
+.venv/Scripts/activate
+
+# check directory structure
+echo "check_docs_frontmatter.py..."
+python3 -u helpers/check_docs_frontmatter.py
+
+# generate doc_sidebar.yaml
+echo "docs_sidebar.py..."
+python3 -u helpers/docs_sidebar.py
+
+# build
+hugo build --themesDir .. --destination ../public
+
+# checkout branch gh-pages
+git checkout gh-pages
+git rm -rf *
+cp -r ../public/* .
+git add .
+git commit -m "Rebuild site"
+git push origin gh-pages --force
+# checkout branch main
+git checkout main
+
+echo "Deploy to gh-pages complete !"
